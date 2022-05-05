0ffadf05e5aa2bf16f873510e6d5159db34c582a
Stanley <95464654+StanleyTC@users.noreply.github.com>
2022-05-05 20:57:19 -0300

    aulas 9 e 10

---
+++ Aula10DesenvolvimentoWeb.cs
@@ -0,0 +1,16 @@
+//na aula de hoje iremos ver desenvolvimento web e nuvem
+//introdução ao Azure: plataforma de nuvem pública da Microsoft. temos lá o Paas, IaaS e recuros de 
+//serviço de banco de dados gerenciados 
+//nuvem: servidores remotos ao redor do mundo com maquinas virtuais ou reais
+//geralmente servidor tem um numero de IP, mas é horrivel ter que acessar algo por IP, então temos que
+//configurar um dominio na web para linkar com o IP da rede e assim ficar mais viavel 
+//exemplo: um site é um dominio que ta redirecionando para um IP de rede
+//se um site nao tiver https, desconfiar da segurança dele
+
+//azure - conta gratuita
+
+
+//agora nao teremos mais console, teremos algo referente a web
+//precisamos da ferramenta de desenvolvimento web do visual studio
+
+//aula incompleta - continuar amanha
---
+++ Aula9LerDadosArquivoLocal.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//aula incompleta, será refeita futuramente, ignorar essa aula por enquanto
+
+namespace Aula1HelloWorld //linha 17 e 18
+{
+    class Aula1HelloWorld //linha 17 e 18
+    {
+        static void Main (string[] args) 
+        {
+            //ler dados de arquivo local.
+            string enderecoArquivo = "";
+
+
+        }
+    }
+
+
+}
